d27bac1ea632fbdb99e9604106edda0653232f5f
Lucas suzuki <lucas14112003@gmail.com>
2022-07-21 09:42:28 -0300

    aula Exibindo HTML

--- aulaEXPRESS/index.js
+++ aulaEXPRESS/index.js
@@ -3,15 +3,20 @@ const app = express();
 
 
 app.get("/", function(req, res) {
-    res.send("Servidor Funcionando, Seja Bem-Vindo")
+    res.sendFile(__dirname+ "/frontEnd/index.html");
 })
 
 app.get("/sobre", function(req,res){
-    res.send("Meu nome: Lucas Suzuki")
+    res.sendFile(__dirname +"/frontEnd/sobre.html");
 })
 
 app.get("/blog", function(req,res){
     res.send("Bem vindo ao meu blog")
+})
+
+app.get('/ola/:nome/:cargo/:cor', function(req, res){
+    res.send("<h1>ola "+req.params.nome+"</h1> "+ "<h2> Seu Cargo e: "+req.params.cargo+"</h2> "+"<h3> Sua cor favorita e: "+req.params.cor+"</h3>")
+    
 })
 
 
